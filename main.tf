9d1741e13df93c44ac7671a2190c122b920693f0
mikey1 <mikey1997@gmail.com>
2023-02-17 13:21:31 +0530

    added vpcs in different network

--- main.tf
+++ main.tf
@@ -8,7 +8,7 @@ resource "aws_vpc" "myvpc" {
 
 resource "aws_subnet" "subnets" {
     count = length(var.subnet_details.Name)
-    vpc_id = aws_vpc.myvpc.id[0]
+    vpc_id = aws_vpc.myvpc.id
     cidr_block = cidrsubnet(var.vpc_details.cidr_block[0],8,count.index)
     tags = {
         Name = var.subnet_details.Name[count.index]
@@ -17,7 +17,7 @@ resource "aws_subnet" "subnets" {
 
 resource "aws_subnet" "subnets2" {
     count = length(var.subnet_details1.Name)
-    vpc_id = aws_vpc.myvpc.id[1]
+    vpc_id = aws_vpc.myvpc.id
     cidr_block = cidrsubnet(var.vpc_details.cidr_block[1],8,count.index)
     tags = {
         Name = var.subnet_details1.Name[count.index]
