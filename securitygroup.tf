f62789ed0f48e661a81433fb6eee71149885a55e
mikey1 <mikey1997@gmail.com>
2023-02-17 19:47:42 +0530

    added vpcs in different network

--- securitygroup.tf
+++ securitygroup.tf
@@ -1,7 +1,7 @@
 resource "aws_security_group" "my-sg" {
   name        = "mikey-sg"
   description = "Allow TLS inbound traffic"
-  vpc_id      =  var.vpc_details.cidr_block.id
+  vpc_id      =  module.vpc.vpc_id
 
   ingress {
     from_port        = 3306
