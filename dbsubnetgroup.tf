4466595ba96be2b170e624446db5f7f8546c5930
mikey1 <mikey1997@gmail.com>
2023-02-17 19:59:59 +0530

    added vpcs in different network

--- dbsubnetgroup.tf
+++ dbsubnetgroup.tf
@@ -1,6 +1,6 @@
 resource "aws_db_subnet_group" "dbsubnetgroup" {
     name = "mikeydbsubnetgroup"
-    subnet_ids = [module.vpc.private_subnets[0].id,module.vpc.private_subnets[1].id]
+    subnet_ids = [module.vpc.private_subnets[0],module.vpc.private_subnets[1]]
 }
 
 
